4de057920ca734bf5a646a36165a1d99ad490b61
Mukosame <mukosame@gmail.com>
2016-01-11 21:32:29 +0800

    revise on I/O, edge_h, and prefix lines

--- rip.m
+++ rip.m
@@ -3,16 +3,37 @@
 % Author: Mukosame  <mukosame@gmail.com>
 % https://github.com/Mukosame/RIP-Peak
 
-[ndata,filename,alldata]=xlsread('ripname.xlsx');
-z_mm = cell(length(alldata),1);
+%[ndata,filename,alldata]=xlsread('ripname.xlsx');
+
+cell = 'B'; %Please choose the cell type, very important!
+prefix = 10; %THIS IS FOR REMOVING THE ULTRA POINTS IN THE CENTER
+suffix = 2; %1/suffix the height's width
+Tag = 'I';% I for Inlet, O for Outlet
+
+direction = '.\';
+alldata = dir(fullfile(direction, '*.RIP'));
+for i = 1:length(alldata)
+    filename{i} = alldata(i).name;
+end
+z_mm = zeros(length(alldata),1);
 cd = zeros(length(alldata),1);
 dn = zeros(length(alldata),1);
 na = zeros(length(alldata),1);
 od = zeros(length(alldata),1);
 otc = zeros(length(alldata),1);
 
+if cell == 'B'
+    ledge = 1000; redge = 2500;
+    edgel = 5000; edger = 6500;
+end
+
+if cell == 'C'
+    ledge = 1000; redge = 2500;
+    edgel = 5000; edger = 6500;
+end
+
 if 1
-    for n=1:length(alldata)
+    for n=1:length(filename)
         figure(n)
         temp = importdata(char(filename(n)),'	',2);
         info = temp.textdata(1,1);
@@ -22,8 +43,12 @@ if 1
         %z_mm(n,1) = {spinfo{1,1}{1,7}};%OK...
         z_mm = spinfo{1,1}{1,7};
         %deg(n,1) = spinfo{1,1}{1,9};
-        position = strcat(id,z_mm, 'mm')
-        %position = strcat(id ,string(z_mm) , 'mm from outlet');
+        if Tag == 'O'
+            position = strcat(id,z_mm, 'mm from inlet');
+        end
+        if Tag == 'I'
+            position = strcat(id ,string(z_mm) , 'mm from outlet');
+        end
         %plot??
         set(gca, 'fontsize', 22)
         linespace = temp.data(2,1)-temp.data(1,1); 
@@ -31,9 +56,11 @@ if 1
         ymin = min (temp.data(3000:4150,2));
         plot (temp.data(:,1), temp.data(:,2), 'LineWidth', 1, 'Color', 'blue')
         len = round(length(temp.data(:,2))/2);
-        %?????????
+        %%%%%%%%%%%%%%%%%%%%%
+        %?FIND CORE DIAMETER
+        %%%%%%%%%%%%%%%%%%%%%
         if 1
-        hh = max(temp.data((len-300):len,2))/5;
+        hh = mean(temp.data((len-100):len,2))/2;
         sn = find(temp.data(len-300:len+300,2)>=hh);
         l=length(sn);
         cleftx = temp.data(len-300+sn(1),1);
@@ -46,9 +73,11 @@ if 1
         text(crightx+0.1,hh,cored,'FontSize',16)
         hold off;
         end
+        %%%%%%%%%%%%%%%%%%%%
         %calculate \Delta n
+        %%%%%%%%%%%%%%%%%%%%
         if 1
-            uppr = mean(temp.data((len-250+sn(1)):len-20,2));
+            uppr = mean(temp.data((len-250+sn(1)):len-prefix,2));
             lower = mean(temp.data(len-1000:len-300,2));
             uleftx = temp.data(len-1100,1);
             urightx = temp.data(len-100,1);
@@ -70,26 +99,24 @@ if 1
             hold off
         end
         if 1
-        %??????????
-        hh = max(temp.data(900:6140,2))/1.5;
-        leftx = find(temp.data(:,2)==ymax);
-        leftx = leftx(1,1);
-        if (leftx > len)
-            leftx = length(temp.data(:,2))-leftx;
-        end
-        rightx = length(temp.data(:,2))-leftx;
-        sn = find(temp.data(leftx:rightx,2)<=hh);
-        l=length(sn);
-        oleftx = temp.data(leftx+sn(1),1);
-        orightx = temp.data(leftx+sn(l),1);
+        %%%%%%%%%%%%%%%%%%%%
+        %OUTER DIAMETER
+        %%%%%%%%%%%%%%%%%%%%
+        edge_h = max(temp.data(ledge:redge,2));
+        leftx = find(temp.data(ledge:redge,2)>=edge_h/1.5);
+        leftx = leftx(length(leftx))+ledge;
+        rightx = find(temp.data(edgel:edger,2)>=edge_h/1.5);
+        rightx = rightx(1)+edgel;
+        oleftx = temp.data(leftx,1);
+        orightx = temp.data(rightx,1);
         od(n) = orightx - oleftx;%outer diameter
         otc(n) = od(n)/cd(n);
         outd = strcat('OD=', num2str(od(n),4), 'mm');
         ymax = max(temp.data(len-300:len,2));
         hold on
         %plot (oleftx:0.1:orightx,hh,'--r');
-        plot([oleftx,orightx],[ymax*1.2,ymax*1.2],'--r');
-        text(-1.2,ymax*1.2+1e-4,outd,'FontSize',16)
+        plot([oleftx,orightx],[min(ymax*1.2,edge_h),min(ymax*1.2,edge_h)],'--r');
+        text(-1.2,min(ymax*1.2,edge_h)+1e-4,outd,'FontSize',16)
         hold off
         end
         %save??
@@ -99,7 +126,7 @@ if 1
         title(position,'fontsize', 22);
         xlabel('Radius [mm]', 'fontsize', 26);
         ylabel('\Delta n', 'fontsize', 26);
-        picname = strcat(position, '.png');
+        picname = strcat(position, '.png')
         figname = strcat(position, '.fig');
         saveas(gcf,picname,'png')
         saveas(gcf,figname,'fig')
